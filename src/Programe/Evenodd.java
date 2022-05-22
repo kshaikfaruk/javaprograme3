6dd4219f988f4c65da9590238fe0baaf066a83d1
shaikfaruk <shaikfaruk04@gmail.com>
2022-05-22 15:51:26 +0530

    secondpush

--- src/Programe/Evenodd.java
+++ src/Programe/Evenodd.java
@@ -4,19 +4,15 @@ public class Evenodd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    int a[]={1,2,3,4,5,4,65,7};
-    for(int i=0;i<a.length;i++){
-    	if(a[i]% 2==0){
-    		
-    	
-    	System.out.println("evennumber"+a[i]);
-    	
-    }else{
-    	System.out.println("oddnumber"+a[i]);
-    }
-    	
-    	
-    }
+   int a[]={1,2,3,4,5,6,65,7};
+   for(int i=0;i<a.length;i++){
+	   if(a[i]%2==0){
+		   System.out.println("evennumber"+a[i]);
+	   }else{
+		   System.out.println("oddnumber"+a[i]);
+		   
+	   }
+   }
 	}
 
 }
